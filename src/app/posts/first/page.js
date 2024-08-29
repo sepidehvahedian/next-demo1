03ff571718d3bbdc4648014ddaf13c362b22f220
sepideh <sepideh0771@gmail.com>
2024-08-29 13:00:22 +0330

    first commit

---
+++ src/app/posts/first/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FirstPostPage() {
+  return <div>FirstPostPage</div>;
+}
+
+export default FirstPostPage;
---
+++ src/app/posts/second/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SecondPostPage() {
+  return <div>SecondPostPage</div>;
+}
+
+export default SecondPostPage;
